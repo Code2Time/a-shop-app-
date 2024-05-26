d6439e96e083b7f55708f4fe33ea1e33e506bb62
Amir-Thp <code2time@gmail.com>
2024-05-26 22:07:52 +0330

    create basic component (HEAD item)

---
+++ src/components/Head/Head.tsx
@@ -0,0 +1,35 @@
+import { AiOutlineInstagram } from "react-icons/ai";
+import "./Head.css";
+import { TiSocialLinkedin } from "react-icons/ti";
+import { FaFacebookF, FaPhoneAlt, FaTwitter } from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
+
+function Head() {
+  return (
+    <>
+      <div className="hidden md:block">
+        <div className=" text-blue-four   flex justify-around items-center h-7   ">
+          <div className="flex justify-center items-center gap-4 ">
+            <AiOutlineInstagram size={15} />
+            <TiSocialLinkedin size={15} />
+            <FaFacebookF size={15} />
+            <FaTwitter size={15} />
+          </div>
+          <div className=" flex  items-center justify-center gap-7">
+            <div className="flex  items-center justify-center gap-2 cursor-pointer">
+              <span className="text-xs">code2time@gmail.com</span>
+              <CgMail size={20} />
+            </div>
+            <div className=" flex  items-center justify-center gap-2 cursor-pointer">
+              <span className="text-xs">98-933-321-87-66</span>
+              <FaPhoneAlt size={15} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
+
+export default Head;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,15 @@
+import Head from "../../components/Head/Head"
+
+
+function Home() {
+  return (
+    <>
+      <section>
+        <Head />
+      </section>
+      home page
+    </>
+  )
+}
+
+export default Home
