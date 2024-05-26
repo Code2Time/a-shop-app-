19f48ffb25200f60df61ade5a19cb1c784c2a568
Amir-Thp <code2time@gmail.com>
2024-05-26 22:06:54 +0330

    custom some item in tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/home/Home'
+
 
 function App() {
-  
-
   return (
-    <>
-     <h1 className='text-center text-4xl'>home page</h1>
-    </>
+  <Routes>
+    <Route path='/' element={<Home />} />
+  </Routes>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {screens: {
+      'sm': '380px',
+      // => @media (min-width: 380px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    colors : {
+      'my-white' : '#ffffff',
+      'blue-one' : '#EDF3FB',
+      'blue-two' : '#CBE3EF',
+      'blue-three' : '#5AA8D6',
+      'blue-four' : '#3A4163',
+    },
+    backgroundColor : {
+      'button' : '#00abe1'
+    },
+
+   
+  },
+    
   },
   plugins: [],
 }
