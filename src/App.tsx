32baa8fc7038ce30deb6735b1bf577d0b9fe4cbe
Amir-Thp <code2time@gmail.com>
2024-05-30 17:33:31 +0330

    creat shopping cart context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './pages/home/Home'
-import PageNotFound from './pages/pagenotfound/PageNotFound'
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import PageNotFound from "./pages/pagenotfound/PageNotFound";
+import {  ShoppingCardProvider } from "./context/ShppingCartContext";
 
 function App() {
   return (
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path='*' element={<PageNotFound />} />
-  </Routes>
-  )
+    <ShoppingCardProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </ShoppingCardProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/context/ShppingCartContext.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface ShoppingCardProvider {
+  children: React.ReactNode;
+}
+
+interface ShopingCardContext {
+    user : ShoppingUser[]
+}
+
+interface ShoppingUser{
+    
+image: string
+email: string
+id : number
+name : string
+
+
+
+
+}
+
+export const ShopingCardContext = createContext(
+  {} as ShopingCardContext
+); /*create context*/
+
+export const useShopingCardContext = () => {
+  return useContext(ShopingCardContext);
+};
+
+export function ShoppingCardProvider({ children }: ShoppingCardProvider) {
+
+
+    const [user , setUser] = useState<ShoppingUser[]>([])
+
+useEffect(()=>{
+axios.get('https://api.escuelajs.co/api/v1/categories')
+.then(res => setUser(res.data))
+},[])
+console.log(user)
+
+    return(
+        <ShopingCardContext.Provider value={{user}}>
+            {children}
+        </ShopingCardContext.Provider>
+    )
+}
