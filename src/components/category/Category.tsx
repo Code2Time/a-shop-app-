bb66601cca7a745a7ba7a02c5fc386321e89cd4a
Amir-Thp <code2time@gmail.com>
2024-06-02 12:22:07 +0330

    update category style

--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -16,10 +16,10 @@ const Category = () => {
 
   return (
     <div className="grid grid-cols-12 gap-2 mt-14 p-10 sm:p-0">
-    <h1 className="dark:text-blue-two text-blue-four col-span-12 brand-text flex items-center justify-start border-l-4 border-blue-500 p-2 dark:border-l-0 my-2">
+    <h1 className="dark:text-blue-two text-blue-four col-span-12 brand-text flex items-center justify-start border-l-4 border-blue-500 p-2 dark:border-l-0 my-2  rounded-b rounded-t">
             <img
               className="h-auto w-5  sm:w-12 hidden dark:block opacity-70"
-              src="https://codenight.ir/_next/image?url=%2Fassets%2Fimages%2FtitleCircleRed.png&w=128&q=75"
+              src="https://codenight.ir/_next/image?url=%2Fassets%2Fimages%2FtitleCirclePink.png&w=128&q=75"
               alt=""
             />
             <span className="poppins sm:text-xl">Categorys</span>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -26,7 +26,7 @@ function Home() {
           <Secondslideshow />
         </div>
         <div className="mt-20 h-autow-full  grid grid-cols-12 gap-5  ">
-          <h1 className="dark:text-blue-two text-blue-four col-span-12 brand-text flex items-center justify-start border-l-4 border-blue-500 p-2 dark:border-l-0  my-2 ">
+          <h1 className="dark:text-blue-two text-blue-four col-span-12 brand-text flex items-center justify-start border-l-4 border-blue-500 p-2 dark:border-l-0  my-2 rounded-b rounded-t ">
             <img
               className="h-auto w-5  sm:w-12 hidden dark:block opacity-70"
               src="https://codenight.ir/_next/image?url=%2Fassets%2Fimages%2FtitleCircleRed.png&w=128&q=75"
