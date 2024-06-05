b0c1826043e18b57745e043b1a707edc578fd72c
Amir-Thp <code2time@gmail.com>
2024-06-05 13:25:35 +0330

    create scroll to top component and add to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Home from "./pages/home/Home";
 import PageNotFound from "./pages/pagenotfound/PageNotFound";
 import {  ShoppingCardProvider } from "./context/ShppingCartContext";
 import Categories from "./pages/categories/Categories";
+import ScrollToTop from "./scroll/ScrollToTop";
 
 function App() {
   return (
     <ShoppingCardProvider>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
