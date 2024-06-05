668c899f6d020af9cc83cf1efaf88e2c51dbc465
Amir-Thp <code2time@gmail.com>
2024-06-05 08:56:46 +0330

    clean products item

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,65 +1,12 @@
-import "./Products.css";
-import ProductsItem from "../productsitem/ProductsItem";
-import ReactPaginate from "react-paginate";
-import { IoArrowRedoOutline, IoArrowUndoOutline } from "react-icons/io5";
-import { Idatapaginat, Iproducts } from "../types/Iproducts";
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-
-
-
+import React from 'react'
+import ProductsItem from '../productsitem/ProductsItem'
 
 function Products() {
-
-    const[products , setProducts]=useState<Iproducts[]>([])
-
-    // const fetchproducts = (pageNumber:number)=>{
-   
-    
-    // }
-
-  // const handlePageClick = (data:Idatapaginat) => {
-    // let pageNumber :number = (data.selected + 1)*10
-    // fetchproducts(pageNumber)
-
-    // console.log(pageNumber)
-  // };
-
-
-  useEffect(()=>{
-//  fetchproducts()
-axios.get('https://dummyjson.com/products/category/smartphones')
-.then(res =>setProducts(res.data.products))
-console.log(products)
-  },[])
-
   return (
-  <><div className="grid grid-cols-12 gap-1 mt-32">
-  {products.map(product=>(
-    <ProductsItem {...product} />
-  ))}
-    
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel={<IoArrowRedoOutline fontSize={20} />}
-        // onPageChange={handlePageClick}
-        pageCount={5}
-        previousLabel={<IoArrowUndoOutline fontSize={20} />}
-        containerClassName="paginat-container"
-        pageClassName="page-item"
-        nextClassName="next-item"
-        previousClassName="prev-item"
-        pageLinkClassName="page-link"
-        nextLinkClassName="next-link"
-        previousLinkClassName="prev-link"
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        activeClassName="activeit"
-      />
+    <div>
+      <ProductsItem />
     </div>
-    </>
-  );
+  )
 }
 
-export default Products;
+export default Products
--- src/components/productsitem/ProductsItem.tsx
+++ src/components/productsitem/ProductsItem.tsx
@@ -1,16 +1,9 @@
+import React from 'react'
 
-import { Iproducts } from '../types/Iproducts'
-
-function ProductsItem({thumbnail , price , title , images}:Iproducts) {
+function ProductsItem() {
   return (
-    <div className='productsItem-container flex flex-col text-center col-span-12 sm:col-span-4 md:col-span-3 border bg-my-white dark:bg-transparent'>
- <div className='product-img w-full h-[70%] p-5 '>
-    <img className='w-full h-full rounded-sm' src={images[2]} alt="product" />
- </div>
- <div className='product-desc my-4 text-blue-600'>
-<p>{title}</p>
- </div>
- <div className='product-price my-4 text-blue-600'>{price}toman</div>
+    <div>
+      product items
     </div>
   )
 }
