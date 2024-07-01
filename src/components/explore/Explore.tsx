edc83b645ddbf05798214bc595248cdc5ca22ba4
Amir-Thp <code2time@gmail.com>
2024-07-01 09:22:29 +0330

    update link in explore component

--- src/components/explore/Explore.tsx
+++ src/components/explore/Explore.tsx
@@ -9,7 +9,7 @@ function Explore() {
             <p className='text-gray-700 dark:text-blue-four poppins text-justify p-1 mt-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique aliquam repellendus veniam dicta possimus, dolores fugit nostrum quod <span className="sm:hidden md:block">provident et recusandae aperiam culpa, modi facilis? Unde dignissimos voluptatem ad eveniet</span></p>
             <p className='text-gray-700 dark:text-blue-four poppins text-justify p-1 hidden md:block'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique aliquam repellendus veniam dicta possimus, dolores fugit nostrum quod provident et recusandae aperiam culpa, modi facilis? Unde dignissimos voluptatem ad eveniet.</p>
             <p className='text-gray-700 dark:text-blue-four poppins text-justify p-1 hidden md:block'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique aliquam repellendus veniam dicta possimus, dolores fugit nostrum quod provident et recusandae aperiam culpa, modi facilis? Unde dignissimos voluptatem ad eveniet.</p>
-            <Link to='/products' className="mt-2 md:mt-14" >
+            <Link to='/store' className="mt-2 md:mt-14" >
             <button className="transition-all w-[30%] p-2 mt-4 sm:w-[60%] md:w-[50%] bg-my-white dark:bg-transparent dark:hover:shadow-md dark:hover:shadow-blue-three text-blue-four hover:text-my-white hover:bg-blue-three dark:hover:bg-transparent    dark:text-blue-three  rounded-md text-xs sm:text-base dark:border-b-blue-two shadow-sm  shadow-blue-three">see more...</button>
             </Link>
         </div>
