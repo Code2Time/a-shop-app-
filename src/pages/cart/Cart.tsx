c4c7263734365f6bc616e05d5423d69417d7c1c4
Amir-Thp <code2time@gmail.com>
2024-07-06 23:33:44 +0330

    update  Cart page ui

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,6 +1,6 @@
 import Head from "../../components/Head/Head";
 import Navbar from "../../components/navbar/Navbar";
-import logo from '../../assets/img/shop-high-resolution-logo-transparent (1).png'
+import logo from '../../assets/img/shop-high-resolution-logo-transparent.png'
 import { FaCcPaypal, FaCcVisa, FaLongArrowAltRight } from "react-icons/fa";
 function Cart() {
   return (
@@ -11,10 +11,10 @@ function Cart() {
       </section>
       <div className="cart-container w-full h-auto">
         <div className="cart grid grid-cols-12 gap-2  w-10/12 max-w-[1500px] mx-auto  p-5 mt-32">
-          <div className="card-types-container col-span-12  md:col-span-5 border  h-auto order-2 rounded-sm flex flex-col justify-center items-center gap-5 p-2">
+          <div className="card-types-container col-span-12  md:col-span-5   h-auto order-2 rounded-sm flex flex-col justify-center items-center gap-5 p-2">
             <div className=" flex flex-row justify-around items-center h-9 w-full">
               <h1 className="dark:text-my-white poppins text-blue-four  ">Cart Details</h1>
-              <img className="size-12 md:size-16 cursor-pointer" src={logo} alt="logo" />
+              <img className="size-12 md:size-16 cursor-pointer transition-all hover:scale-105" src={logo} alt="logo" />
             </div>
             <div className="card-type flex flex-col justify-start w-full gap-5">
               <h2 className="uppercase dark:text-blue-three poppins text-blue-four sm:text-xl">card types</h2>
@@ -30,11 +30,11 @@ function Cart() {
                 <input required type="text"className="rounded-md w-[40%] mx-auto  focus:outline-none placeholder:text-black placeholder:text-sm poppins bg-slate-400 focus:bg-slate-500 text-left py-1 px-2   hover:cursor-pointer" placeholder="Expirition" />
                 <input required type="number" className="rounded-md w-[30%] mx-auto  focus:outline-none placeholder:text-black placeholder:text-sm poppins bg-slate-400 focus:bg-slate-500 text-left py-1 px-2   hover:cursor-pointer" placeholder="Cvv"/>
               </div>
-              <hr />
+              <hr className="my-5 sm:my-10" />
             </div>
             <div className="w-full flex flex-col justify-center items-center">
-                <h2 className="dark:text-my-white poppins text-blue-four text-2xl">Total : <span>254 $</span></h2>
-                <button className="flex justify-center items-center gap-2 text-xl dark:text-my-white poppins text-blue-four" type="submit"><span>CheckOut</span><span><FaLongArrowAltRight /></span></button>
+                <h2 className="dark:text-my-white poppins text-blue-four sm:text-2xl">Total : <span>254 $</span></h2>
+                <button className="flex justify-center items-center gap-2 transition-all w-[60%] p-2 mt-4 sm:w-[60%] md:w-[50%] bg-blue-100 dark:bg-transparent dark:hover:shadow-md dark:hover:shadow-blue-three text-blue-four hover:text-my-white hover:bg-blue-three dark:hover:bg-transparent    dark:text-blue-three  rounded-md text-xs sm:text-base dark:border-b-blue-two shadow-sm  shadow-blue-three" type="submit"><span>CheckOut</span><span><FaLongArrowAltRight /></span></button>
             </div>
           </div>
           <div className="cart-items-container col-span-12  md:col-span-7 border h-auto rounded-sm"></div>
