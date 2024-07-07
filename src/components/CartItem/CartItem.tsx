ce5d5b8c40e6f19a82db849497977320de8485dd
Amir-Thp <code2time@gmail.com>
2024-07-07 10:51:06 +0330

    set dynamic cartitem

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -1,6 +1,9 @@
 import { FaTrashAlt } from "react-icons/fa";
 import "./CartItem.css";
 import { useShopingCardContext } from "../../context/ShppingCartContext";
+import { ProductsData } from "../../pages/Store/ProductsData";
+import { useEffect, useState } from "react";
+import { Iallproducts } from "../types/Iproducts";
 
 interface Iproduct {
   id: number;
@@ -8,7 +11,13 @@ interface Iproduct {
 }
 
 function CartItem({ id, qty }: Iproduct) {
-  
+
+    const [product , setProduct] = useState<Iallproducts>()
+    useEffect(()=>{
+    const selected:any = ProductsData.find(item => item.id == id)
+    setProduct(selected)
+},[])
+
   const {
     HandleDecreaseProductQty,
     HandleIncreaseProductQty,
@@ -18,20 +27,23 @@ function CartItem({ id, qty }: Iproduct) {
   return (
     <div className="CartItem-container w-full overflow-x-auto h-auto mx-auto flex flex-row justify-evenly items-center  gap-5   rounded-lg px-3 py-5 my-2">
       <img
-        src="https://4kwallpapers.com/images/walls/thumbs_3t/17401.jpg"
+        src={product?.img}
         alt="Cart Item"
         className="size-30 sm:size-24 rounded-sm cursor-pointer"
       />
       <div>
         <h1 className="uppercase line-clamp-1 head dark:text-my-white">
-         name
+   {product?.name}
         </h1>
-        <p className="uppercase line-clamp-1 poppins dark:text-slate-400">
-          description
+        <p className="uppercase line-clamp-2 poppins dark:text-slate-400">
+         {product?.description}
         </p>
       </div>
       <h1 className="dark:text-blue-three text-xl head">{qty}</h1>
-      <h1 className="dark:text-blue-three smd:text-xl head ">$12247</h1>
+      <div className="flex flex-col justify-center items-center">
+      <h2 className="dark:text-blue-three smd:text-lg head line-through decoration-red-500 ">${product?.price}</h2>
+      <h1 className="dark:text-blue-three smd:text-xl head ">${product?.discount}</h1>
+      </div>
       <div className="control-buttons flex ">
         <button
           onClick={() => {
